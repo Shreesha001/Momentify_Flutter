7d6c2bb0c268cd9f96dfd022d7700b8bdae87ce4
Shreesha Shetty <shettyshreesha552@gmail.com>
2025-03-12 02:33:06 +0530

    Work On User Data State Management

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/widgets.dart';
 import 'package:instagram_flutter/resources/storage_methods.dart';
 import 'package:instagram_flutter/models/user.dart'
@@ -11,6 +12,13 @@ import 'package:instagram_flutter/models/user.dart'
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<model.User> getUserDetails() async {
+    User currentUser = _auth.currentUser!;
+    DocumentSnapshot snap =
+        await _firestore.collection('users').doc(currentUser!.uid).get();
+    return model.User.fromSnap(snap);
+  }
 
   Future<String> signUpUser({
     required String username,
