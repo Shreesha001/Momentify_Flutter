abcb43499d940c810f821dddfab2ab6aec5281e9
Shreesha Shetty <shettyshreesha552@gmail.com>
2025-03-14 00:29:53 +0530

    Display post data from firebase on feed

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:instagram_flutter/utils/colors.dart';
+import 'package:instagram_flutter/widgets/post_card.dart';
 
 class FeedScreen extends StatelessWidget {
   const FeedScreen({super.key});
@@ -17,8 +19,28 @@ class FeedScreen extends StatelessWidget {
           height: 32,
         ),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.messenger_outline)),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.messenger_outline),
+          ),
         ],
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (
+          context,
+          AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot,
+        ) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder:
+                (context, index) =>
+                    PostCard(snap: snapshot.data!.docs[index].data()),
+          );
+        },
       ),
     );
   }
