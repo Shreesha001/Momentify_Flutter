f88592c0e69b9cf3423f52602db46f989589bed6
Shreesha Shetty <shettyshreesha552@gmail.com>
2025-03-15 16:56:08 +0530

    Add search feature

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,82 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_flutter/utils/colors.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController searchController = TextEditingController();
+  bool isShowUsers = false;
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    searchController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: TextFormField(
+          controller: searchController,
+
+          decoration: InputDecoration(
+            // floatingLabelBehavior: FloatingLabelBehavior.never,
+            labelText: 'Search for a user',
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.blue,
+                width: 2.0,
+              ), // Blue border when focused
+            ),
+          ),
+          onFieldSubmitted: (String value) {
+            setState(() {
+              isShowUsers = true;
+            });
+          },
+        ),
+      ),
+
+      body:
+          isShowUsers
+              ? FutureBuilder(
+                future:
+                    FirebaseFirestore.instance
+                        .collection('users')
+                        .where(
+                          'username',
+                          isGreaterThanOrEqualTo: searchController.text,
+                        )
+                        .get(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  return ListView.builder(
+                    itemCount: (snapshot.data! as dynamic).docs.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage(
+                            (snapshot.data! as dynamic).docs[index]['photoUrl'],
+                          ),
+                        ),
+                        title: Text(
+                          (snapshot.data! as dynamic).docs[index]['username'],
+                        ),
+                      );
+                    },
+                  );
+                },
+              )
+              : Text("POSTS"),
+    );
+  }
+}
